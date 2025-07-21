c4cb2ddacdd54a67d92e8a05e84198a3b1bee890
cloudQuest7 <anjalijayakumar79@gmail.com>
2025-07-22 00:27:16 +0530

    implement cocktail section

--- aguamo/src/app/components/Cocktails.tsx
+++ aguamo/src/app/components/Cocktails.tsx
@@ -1,32 +1,69 @@
 import React from 'react'
 import Image from 'next/image'
 import { cocktailLists } from '../../../constants'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
 
 const Cocktails = () => {
+    useGSAP(() => {
+        const parallaxTimeLine = gsap.timeline({
+            scrollTrigger: {
+                trigger: '#cocktails',
+                start: 'top 30%',
+                end: 'bottom 80%',
+                scrub: true,
+            }
+         })
+         parallaxTimeLine
+         .from('#c-left-leaf', {
+            x: -100, y:100
+        })
+        .from('#c-right-leaf', {
+            x: 100, y: 100
+        })
+    })
+
+
   return (
     <section id="cocktails" className='noisy'>
         <Image 
         src="/images/cocktail-left-leaf.png" 
         alt="l-leaf"
          id="c-left-leaf"
-         width={300} 
-         height={300}
+         width={600} 
+         height={600}
          />
 
         <Image
          src="/images/cocktail-right-leaf.png"
           alt="r-leaf"
            id="c-right-leaf"
-              width={300}
-              height={300} 
+              width={600}
+              height={600} 
               />
         <div className='list'>
             <div className='popular'>
-                <h2> Most Popular Cocktails here:</h2>
+                <h2> Most Popular Cocktails:</h2>
                 <ul>
                     {cocktailLists.map(({name, country, detail, price}) => (
                        <li key={name}>
                         <div className="md:me-28">
+                            <h3>{name}</h3>
+                            <p>{country} | {detail}</p>
+                        </div>
+                        <span> {price}</span>
+                       </li>
+                    ))}
+                </ul>
+
+            </div>
+
+            <div className='loved'>
+                <h2> Most loved Cocktails:</h2>
+                <ul>
+                    {cocktailLists.map(({name, country, detail, price}) => (
+                       <li key={name}>
+                        <div className="me-28">
                             <h3>{name}</h3>
                             <p>{country} | {detail}</p>
                         </div>
