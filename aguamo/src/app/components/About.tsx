078967633548611adc2b023d974ee3777b531d65
cloudQuest7 <anjalijayakumar79@gmail.com>
2025-07-22 02:22:04 +0530

    About.tsx in progress

--- aguamo/src/app/components/About.tsx
+++ aguamo/src/app/components/About.tsx
@@ -1,6 +1,38 @@
 import React from 'react'
+import Image from 'next/image'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import{ SplitText } from 'gsap/all'
+import { duration } from '@mui/material'
+import { ScrollTimeLine } from 'gsap/ScrollTrigger'
 
 const About = () => {
+    useGSAP(() => {
+      const titleSplit = SplitText.create('#about h2',{
+        type: 'lines'
+      })
+
+    const scrollTimline = gsap.timeline({
+        scrollTrigger: {
+            trigger: '#about',
+            start: 'top center',
+        }
+    })
+
+    scrollTimeline
+    .from(titleSplit.words,{
+        opacity:0,
+        duration: 1,
+        yPercent: 100,
+        ease:'expo.out',
+        stagger:0.02
+    })
+    .from('.top-grid div', '.bottom-grid div',{
+        opacity:0, duration: 1, ease:'power.inOut', stagger:0.04,
+    }, '-=0.5'
+    )})
+
+
   return (
     <div id="about">
     <div className='mb-16 md:px-0 px-5'>
@@ -17,8 +49,67 @@ const About = () => {
                 Every cocktail we serve is a reflection of our obsession with detail - from the first muddle to the final garnish. That care is what turn a simple drink into something truly memorable.
             </p>
         </div>
+        <p className='md:text-3xl text-xl font-bold'>
+            <span>4.5</span>5
+        </p>
+        <p className='text-sm text-white-100'>
+            More than +12000 customers
+        </p>
     </div>
     </div>
+    <div className='top-grid'>
+        <div className='md:col-span-3'>
+            <div className='noisy' />
+                <Image 
+                src="/images/abt1.png"
+                alt="grid-img-1"
+                width={500}
+                height={500}
+                 />
+            </div>
+        {/* </div> */}
+        <div className='md:col-span-6'>
+            <div className='noisy' />
+                <Image 
+                src="/images/abt2.png"
+                alt="grid-img-2"
+                width={500}
+                height={500}
+                 />
+            </div>
+
+            <div className='md:col-span-3'>
+            <div className='noisy' />
+                <Image 
+                src="/images/abt5.png"
+                alt="grid-img-5"
+                width={500}
+                height={500}
+                 />
+            </div>
+    </div>
+
+    <div className='bottom-grid'>
+            <div className='md:col-span-8'>
+            <div className='noisy' />
+                <Image 
+                src="/images/abt3.png"
+                alt="grid-img-3"
+                width={500}
+                height={500}
+                 />
+            </div>
+
+            <div className='md:col-span-4'>
+            <div className='noisy' />
+                <Image 
+                src="/images/abt4.png"
+                alt="grid-img-4"
+                width={500}
+                height={500}
+                 />
+            </div>
+        </div>
     </div>
   )
 }
